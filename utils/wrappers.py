57b513a1dccfc86ffc9a0f4d91cfd39ecd8a405c
xff <sailbafan@gmail.com>
2019-02-08 09:41:54 +0800

    add set_foods method

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -31,6 +31,9 @@ class WrapFrame(gym.ObservationWrapper):
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
         frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_AREA)
         return frame[:, :, None]
+    
+    def set_foods(self, n):
+        self.env.set_foods(n)
 
 
 class MultiWrapFrame(gym.ObservationWrapper):
